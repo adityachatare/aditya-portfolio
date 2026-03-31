a4f98103c4ab2fa12bf95c7f34b2cce2d2322db1
Aditya Chatare <aditya.chatare@brightblu.com>
2026-03-31 15:33:18 +0530

    Changes in about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -42,7 +42,7 @@ const About = () => {
           </h3>
           {/* About Me Paragraph */}
           <p className="text-base sm:text-lg md:text-lg text-gray-400 mb-10 mt-8 leading-relaxed">
-            I am a full-stack mobile app developer with over 4+ years of experience in
+            I am a full-stack mobile app developer with over 5+ years of experience in
             building scalable mobile & web applications. Skilled in both front-end and
             back-end development, I specialize in the Cross Platform, Native and other
             modern technologies to create seamless user experiences and
